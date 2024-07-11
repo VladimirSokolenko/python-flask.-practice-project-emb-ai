db78d8dea9b8d1a00254044a800e92d4007e2543
vladimir.sokolenko <vladimir.sokolenko@gmail.com>
2024-07-11 11:54:00 +0300

    tasks 3,4 done

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from requests.auth import HTTPBasicAuth
 
 def sentiment_analyzer(text_to_analyse):
@@ -9,4 +10,8 @@ def sentiment_analyzer(text_to_analyse):
     auth = HTTPBasicAuth('apikey', 'Hr1P2YsklytsPAUuCSwxBpr6JWNRd3r41lxvFOIc2Ktu')
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     response = requests.post(url + '/v1/analyze?version=2019-07-12', json = myobj, headers=header, auth=auth)
-    return response.text
+    formatted_response = json.loads(response.text)
+    label = formatted_response['sentiment']['document']['label']
+    score = formatted_response['sentiment']['document']['score']
+    # return response.text
+    return {'label':label, 'score':score}
---
+++ tasks3-4.py
@@ -0,0 +1,26 @@
+import json
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+# text = 'I love this new technology.'
+text = "This is fun."
+# text = 'Я люблю этот город'
+
+response = sentiment_analyzer(text)
+
+
+# for not formated response
+# formatted_response = json.loads(response)
+# label = formatted_response['sentiment']['document']['label']
+# score = formatted_response['sentiment']['document']['score']
+# language = formatted_response['language']
+# print(formatted_response['sentiment'])
+# print(f'Document language: {language}')
+# print(f'Document sentiment label: {label}')
+# print(f'Document sentiment score: {score}')
+
+# for formated response
+label = response['label']
+score = response['score']
+print(f'Analysed text: \"{text}\"')
+print(f'Response: {response}')
+print(f'Document sentiment label: {label}')
+print(f'Document sentiment score: {score}')
